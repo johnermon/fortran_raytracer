3e7e0cb5e74034fdac7d00f38f307c84f1f18996
Josie <josiemon@proton.me>
2026-09-20 15:02:44 -0400

    improved cmakelists

--- canvas.c
+++ canvas.c
@@ -1,10 +1,13 @@
+#include <MiniFB.h>
 #include <stdint.h>
-
 #define STB_IMAGE_WRITE_IMPLEMENTATION
+
 #include "stb_image_write.h"
 
+int run_render_loop() {}
+
 int save_canvas(const char *filename, int width, int height,
-                const u_int8_t *pixels) {
+                const unsigned char *pixels) {
 
   return stbi_write_png(filename, width, height, 3, pixels, 3 * width);
 }
