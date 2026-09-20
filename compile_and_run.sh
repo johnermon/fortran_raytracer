b1418f9d1a5b1e91b3ceb0b18c9ad7ef162842f5
Josie <josiemon@proton.me>
2026-09-20 14:17:02 -0400

    deleted compile and run scripts, unneded now

--- compile_and_run.sh
+++
@@ -1,2 +0,0 @@
-#/bin/env bash
-gfortran -funsigned -fno-range-check -O3 -fopenmp -march=native fortran_raytracer.f90 c_canvas.o -o raytracer && OMP_NUM_THREADS=$(nproc) OMP_PROC_BIND=true ./raytracer && timg output.png
--- run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/env bash
-OMP_NUM_THREADS=$(nproc) OMP_PROC_BIND=true ./raytracer && timg output.png
